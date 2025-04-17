01d82bc2cdad5918dac68b76fa388a2fd5e114bf
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-04-16 23:17:59 -0300

    script sobre FOR EACH

---
+++ estrutura_repeticao/for_each.py
@@ -0,0 +1,8 @@
+produtos = ['coca', 'pepsi', 'guaraná', 'sprite', 'fanta']
+texto = 'ronaldorhoney@hotmail.com'
+
+for produto in produtos:
+    print('O produto é {}'.format(produto))
+
+for ch in texto:
+    print(ch)
