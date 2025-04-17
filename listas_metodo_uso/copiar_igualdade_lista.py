eae6b4d9c5100e09223abf79bb14caf6d5c73bde
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-04-16 21:00:18 -0300

    atualizando a lista de estudos

---
+++ listas_metodo_uso/copiar_igualdade_lista.py
@@ -0,0 +1,19 @@
+
+
+lista01 = ['ipad', 'iphone x', 'apple tv',]
+lista02 = lista01
+
+lista01[1] = 'ihpne 11'
+
+print(lista01)
+print(lista02)
+
+# Fazendo cópias de listas:
+
+lista03 = ['Android', 'celular', 'mac book']
+lista04 = lista03.copy()
+
+lista03[1] = 'notbook'
+
+print(lista03)
+print(lista04)
