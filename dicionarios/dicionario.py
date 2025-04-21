ea841473d2a0b84717b80cd262e37f5120f8ff94
ronaldorhoney <ronaldorhoney@hotmail.com>
2025-04-20 23:05:35 -0300

    trabalhando com dicionários em Python

---
+++ dicionarios/dicionario.py
@@ -0,0 +1,8 @@
+mais_vendido = {'tecnologia': 'Iphone x', 'Refrigeracao': 'ar condicionado', 'livro': 'O poder de Python', 'eletrodomestico': 'geladeira'}
+vendas_tecnologia = {'Iphone': 1500, 'samsung': 12000, 'ps5': 14000, 'Ipad': 19600}
+
+qtdd_ps5 = vendas_tecnologia['ps5']
+print(qtdd_ps5)
+
+livro = mais_vendido['livro']
+print(livro)
